c6e888c16ed2308667df314d4bc333e5086b43f1
githubkabir123 <dehan2500@gmail.com>
2025-09-23 16:12:18 +0600

    1.0.2

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
